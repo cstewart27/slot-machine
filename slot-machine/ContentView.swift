c77ef3da5451bdf7e56914838205cf4bec5c98d3
Christopher Stewart <christopherstewartsep@gmail.com>
2022-02-23 13:21:12 -0500

    Initial Commit

---
+++ slot-machine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  slot-machine
+//
+//  Created by Christopher Stewart on 2/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ slot-machine/slot_machineApp.swift
@@ -0,0 +1,17 @@
+//
+//  slot_machineApp.swift
+//  slot-machine
+//
+//  Created by Christopher Stewart on 2/23/22.
+//
+
+import SwiftUI
+
+@main
+struct slot_machineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
